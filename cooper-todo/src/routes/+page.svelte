daf54229b658fe88e6599699221367e21c073c71
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-03-04 23:38:27 +0900

    practice sveltekit

---
+++ cooper-todo/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</svelte:head>
+
+<div style="margin: 0 auto; padding: 20px; width: 700px;">
+    <h2 style="text-align: center;">ToDo List</h2>
+</div>
