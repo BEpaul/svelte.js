8a1d52760defacf309116f9205e16a10f830a558
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-03-06 11:38:22 +0900

    practice sveltekit

--- cooper-todo/src/routes/+page.svelte
+++ cooper-todo/src/routes/+page.svelte
@@ -2,8 +2,54 @@
      <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
 </svelte:head>
 
+<script>
+    let toDoList = [];
+    let textInput = '';
+    
+    function addToDo() {
+        toDoList = [...toDoList, { content: textInput, editing: false, checked: false }]
+        textInput = ''
+    }
+
+    function setEditing(i, isEditing) {
+        toDoList[i].editing = isEditing;
+    }
+
+    function deltetTodo(i) {
+        toDoList.splice(i, 1)
+        toDoList = toDoList;
+    }
+
+    
+</script>
+
 <div style="margin: 0 auto; padding: 20px; width: 700px;">
     <h2 style="text-align: center;">ToDo List</h2>
+    <p>Enter your ToDo here</p>
+    <div style="display: flex;">
+        <input type="text" bind:value={textInput}>
+        <button style="width: 200px" on:click={addToDo}>Add</button>
+    </div>
 </div>
 
-<h3>check list</h3>
+
+{#each toDoList as toDo, i}
+    <div style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;">
+        {#if toDo.editing}
+            <input type="text" bind:value={toDo.content}>
+        {:else}
+            <input type="checkbox" bind:checked={toDo.checked}>
+            <h4 style="flex-grow: 1;">{toDo.content}</h4>
+        {/if}
+        <div style="display: flex;">
+            {#if toDo.editing}
+                <button on:click={() => setEditing(i, false)}>Save</button>
+            {:else}
+                <button on:click={() => setEditing(i, true)}>Edit</button>
+            {/if}
+            <button on:click={() => deltetTodo(i)}>Delete</button>
+        </div>
+    </div>
+    
+{/each}
+
